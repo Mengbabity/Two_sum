01100394cd67eaec79fb23c42e51399a50256cf3
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-20 14:28:40 +0800

    Create two_sum_main.java

---
+++ two_sum_main.java
@@ -0,0 +1,12 @@
+package two_sum;
+
+public class two_sum {
+
+	public static void main(String[] args)
+	{
+		solution Solution=new solution();
+		int[] array={3,2,4};
+		int target=6;
+		Solution.twoSum(array, target);
+	}
+}
